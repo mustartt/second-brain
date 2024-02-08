4f009f04cfe8c67b574f240de169bd162c9784ec
Henry Jiang <h243jian@uwaterloo.ca>
2024-02-08 12:35:24 -0800

    send cloud task to cloud run

--- functions/main.py
+++ functions/main.py
@@ -1,6 +1,6 @@
 import json
 
-import pathlib
+import logging as log
 
 from datetime import datetime
 from flask import jsonify
@@ -9,7 +9,7 @@ from uuid import uuid4
 from firebase_functions import https_fn, storage_fn
 from firebase_admin import initialize_app, firestore
 from firebase_functions.options import CorsOptions
-from google.cloud import storage
+from google.cloud import storage, tasks_v2, logging
 
 from fn_impl.utils import verify_id_token
 
@@ -17,10 +17,32 @@ initialize_app()
 
 
 @storage_fn.on_object_finalized(bucket='speedy-atom-413006.appspot.com')
-def archived_bucket(event: storage_fn.CloudEvent[storage_fn.StorageObjectData]):
-    file_path = pathlib.PurePath(event.data.name)
+def route_ingest_files(event: storage_fn.CloudEvent[storage_fn.StorageObjectData]):
+    logging_client = logging.Client()
+    logging_client.setup_logging()
+    client = tasks_v2.CloudTasksClient()
+    parent = client.queue_path(project='speedy-atom-413006',
+                               location='us-central1',
+                               queue='file-ingest-tasks')
 
-    print(file_path, json.dumps(event.data.metadata))
+    object_id = event.data.name
+    log.info(f'{route_ingest_files.__name__}: sending {object_id} to task queue')
+
+    task = {
+        'http_request': {
+            'http_method': tasks_v2.HttpMethod.POST,
+            'url': 'https://vaultgpt-uhefmk7o7q-uc.a.run.app/api/process-file',
+            'oidc_token': {
+                'service_account_email': '664044881400-compute@developer.gserviceaccount.com'
+            },
+            'headers': {'Content-Type': 'application/json'},
+            'body': json.dumps({'objectId': object_id}).encode()
+        }
+    }
+    task = tasks_v2.Task(mapping=task)
+    response = client.create_task(parent=parent, task=task)
+
+    log.info(f'{route_ingest_files.__name__}: added {response.name} to task queue')
 
 
 @https_fn.on_request(
